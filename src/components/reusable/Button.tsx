9573b3ae8a2ed724ce2bb099430a35190e0349c1
better5afe <abramczyk2704@gmail.com>
2023-09-26 19:27:55 +0200

    Added animations

--- src/components/reusable/Button.tsx
+++ src/components/reusable/Button.tsx
@@ -4,12 +4,9 @@ import './Button.scss';
 
 const Button: React.FC<ButtonProps> = ({ type, text, onClick }) => {
 	return (
-		<button
-			type={type}
-			onClick={onClick}
-			className='button'
-		>
+		<button type={type} onClick={onClick} className='button'>
 			{text}
+			<div className='button__reflection'>&nbsp;</div>
 		</button>
 	);
 };
